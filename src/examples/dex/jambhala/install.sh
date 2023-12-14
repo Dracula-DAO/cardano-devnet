6980a6b66d038d73a649ded287c8b2f5de2bfe97
Mark <mjackson001@gmail.com>
2023-12-14 15:43:40 -0700

    add DEX README

--- src/examples/dex/jambhala/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# This script copies the Jambhala plutus contract and native token script into place
-# in the Jambhala directory. The user needs to make the changes necessary to build and
-# use it (modify the cabal file, git add, etc)
-
-cp BondingCurve.hs $PROJECT_ROOT/src/Contracts
-cp state-token.script $NATIVE_SCRIPTS_PATH
-
