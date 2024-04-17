3f4f80dd627812fc2eb76976e4357e6ac9e90585
cryptophonic <409166+cryptophonic@users.noreply.github.com>
2024-04-17 17:22:14 -0600

    search for blocks, transactions or utxos

--- explorer/src/routes/+layout.server.js
+++ explorer/src/routes/+layout.server.js
@@ -2,6 +2,5 @@ import { loadLatest } from "$lib/server"
 
 export function load() {
   const latest = loadLatest()
-  console.log(JSON.stringify(latest, null, 2))
   return latest
 }
--- explorer/src/routes/search/+page.server.js
+++ explorer/src/routes/search/+page.server.js
@@ -1,9 +1,17 @@
 import { redirect } from '@sveltejs/kit'
 
+import { search } from "$lib/server"
+
 export const actions = { 
   default: async ({ request }) => {
     const data = await request.formData()
     const pattern = data.get('pattern')
-	  redirect(303, '/block/' + pattern);
+    let url
+    try {
+      url = await search(pattern)
+    } catch (err) {
+      url = "/notfound?" + pattern
+    }
+    redirect(303, url)
   }
 }
