53f8f5d5259918d1d8331edf5858068d7d023987
cryptophonic <409166+cryptophonic@users.noreply.github.com>
2024-05-15 13:41:30 -0600

    add page route handler for blocks

---
+++ explorer/src/routes/(web)/pages/blocks/[page]/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let data 
+</script>
+<div class="card-title">Block page {data.pageIndex}</div>
+<div class="flex flex-wrap grow">
+  {#each data.pageData as blk}
+    <a href="/chain/{blk.height}">
+      <div class="color-block shadow-xl flex flex-col p-4 mr-2">
+        <div>Block {blk.height}</div>
+        <div>Hash {blk.id}</div>
+      </div>
+    </a>
+  {/each}
+</div>
