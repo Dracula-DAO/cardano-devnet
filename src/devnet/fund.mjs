bd7a39349c4390021d57bfbaefdb910cf1f24925
Mark <mjackson001@gmail.com>
2024-05-22 11:34:11 -0600

    rewrite setup script to use lucid funding script

--- src/devnet/fund.mjs
+++ src/devnet/fund.mjs
@@ -1,23 +1,22 @@
 import { C, Lucid } from 'lucid-cardano'
 import { decode as decodeCbor } from 'cbor-x'
-import { LucidProviderFrontend } from './src/lucid-frontend.mjs'
+import { LucidProviderFrontend } from '../lucid-frontend.mjs'
 
 const wallet_name = process.argv[2]
-const to_addr_name = process.argv[3]
-const value = Math.floor(process.argv[4] * 1000000.0)
-console.log("Using wallet: " + wallet_name)
+const value = Math.floor(process.argv[3] * 1000000.0)
+console.log("Funding wallet: " + wallet_name)
 
 const main = async () => {
   const provider = new LucidProviderFrontend("ws://localhost:1338")
   await provider.init()
   const lucid = await Lucid.new(provider, "Custom")
 
-  const cbor = JSON.parse(fs.readFileSync(process.env.KEYS_PATH + "/" + wallet_name + ".skey").toString())
+  const cbor = JSON.parse(fs.readFileSync(process.env.KEYS_PATH + "/faucet" + ".skey").toString())
   const decoded = decodeCbor(Buffer.from(cbor.cborHex, 'hex'))
   const privKey = C.PrivateKey.from_normal_bytes(decoded)
   lucid.selectWalletFromPrivateKey(privKey.to_bech32())
 
-  const toAddr = fs.readFileSync(process.env.ADDR_PATH + "/" + to_addr_name + ".addr").toString()
+  const toAddr = fs.readFileSync(process.env.ADDR_PATH + "/" + wallet_name + ".addr").toString()
   const tx = await lucid.newTx()
     .payToAddress(toAddr, { lovelace: value })
     .complete();
--- src/examples/counter/setup.sh
+++ src/examples/counter/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-tx-submit transfer-owner
-tx-submit transfer-alice
-tx-submit transfer-bob
+fund owner 1000
+fund alice 100
+fund bob 100
 
