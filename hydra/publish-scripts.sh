60a3c3f8c9eb42bcba82bf0378b2785b8cca5363
cryptophonic <409166+cryptophonic@users.noreply.github.com>
2024-10-20 10:15:25 -0600

    initial hydra demo

--- hydra/publish-scripts.sh
+++ hydra/publish-scripts.sh
@@ -3,4 +3,4 @@
 hydra-node publish-scripts \
   --testnet-magic 42 \
   --node-socket $CARDANO_NODE_SOCKET_PATH \
-  --cardano-signing-key $CARDANO_ASSETS_PATH/keys/faucet.skey > $DEVNET_ROOT/hydra/network/scripts_utxo
+  --cardano-signing-key $CARDANO_ASSETS_PATH/keys/faucet.skey > $DEVNET_ROOT/hydra/network/scripts-utxo.txt
