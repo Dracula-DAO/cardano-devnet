35a4e5d5a979d90e582661872d14fe8e1c81abce
cryptophonic <409166+cryptophonic@users.noreply.github.com>
2024-10-20 14:34:50 -0600

    add funding for node participants using unique funds address

--- hydra/setup-peers.sh
+++ hydra/setup-peers.sh
@@ -18,9 +18,13 @@ for name in "$@"; do
   # credentials
   cd $nodedir
   direnv allow
+  cardano-cli address key-gen --verification-key-file ./funds.vk --signing-key-file ./funds.sk
+  cardano-cli address build --verification-key-file ./funds.vk --out-file ./funds.addr
   cardano-cli address key-gen --verification-key-file ./node.vk --signing-key-file ./node.sk
   cardano-cli address build --verification-key-file ./node.vk --out-file ./node.addr
   hydra-node gen-hydra-key --output-file ./hydra
+  fund $(cat ./funds.addr) 1000
+  fund $(cat ./node.addr) 100
   cd - > /dev/null
 
   echo $name $ip >> $peers
