a8afea992da8eaf70b5f6614c1d8787b4c67c3a8
cryptophonic <>
2024-11-26 13:01:51 -0700

    add tr around th / td in TransactionDetail

--- explorer/src/lib/TransactionDetail.svelte
+++ explorer/src/lib/TransactionDetail.svelte
@@ -16,53 +16,57 @@
       <td>
         <table class="table-header-group border-separate border-spacing-4">
           <thead>
-            <th>Inputs</th>
-            <th>Outputs</th>
+            <tr>
+              <th>Inputs</th>
+              <th>Outputs</th>
+            </tr>
           </thead>
           <tbody>
-            <td>
-              <div class="flex flex-col">
-                {#if tx.inputs.length > 0}
-                  {#each tx.inputs as input}
-                    <a class="self-center card flex flex-col flex-nowrap p-2 color-spent-utxo shadow-xl mb-2" href="/utxo/{input.hash[0]}/{input.ref}">
-                      <div class="self-center">{input.hash[1]}#{input.ref}</div>
-                      <div class="self-center">{input.addr[1]}</div>
-                      {#if input.alias !== undefined}
-                        <div class="self-center">{input.alias}</div>
-                      {/if}
-                      <div class="self-center">&#x20B3; {input.value["ada"]} {#if input.tokenCount > 0}+ token(s){/if}</div>
-                    </a>
-                  {/each}
-                {:else}
-                  Genesis transaction 
-                {/if}
-              </div>
-            </td>
-            <td>
-              <div class="flex flex-col">
-                {#each tx.outputs as output}
-                  {#if output.spentBy === "unspent"}
-                    <a class="self-center card flex flex-col flex-nowrap p-2 color-unspent-utxo shadow-xl mb-2" href="/utxo/{tx.id}/{output.ref}">
-                      <div class="self-center">{tx.hash[1]}#{output.ref}</div>
-                      <div class="self-center">Addr {output.addr[1]}</div>
-                      {#if output.alias !== undefined}
-                        <div class="self-center">{output.alias}</div>
-                      {/if}
-                      <div class="self-center">&#x20B3; {output.value["ada"]} {#if output.tokenCount > 0}+ token(s){/if}</div>
-                    </a>
+            <tr>
+              <td>
+                <div class="flex flex-col">
+                  {#if tx.inputs.length > 0}
+                    {#each tx.inputs as input}
+                      <a class="self-center card flex flex-col flex-nowrap p-2 color-spent-utxo shadow-xl mb-2" href="/utxo/{input.hash[0]}/{input.ref}">
+                        <div class="self-center">{input.hash[1]}#{input.ref}</div>
+                        <div class="self-center">{input.addr[1]}</div>
+                        {#if input.alias !== undefined}
+                          <div class="self-center">{input.alias}</div>
+                        {/if}
+                        <div class="self-center">&#x20B3; {input.value["ada"]} {#if input.tokenCount > 0}+ token(s){/if}</div>
+                      </a>
+                    {/each}
                   {:else}
-                    <a class="self-center card flex flex-col p-2 color-spent-utxo shadow-xl mb-2" href="/utxo/{tx.id}/{output.ref}">
-                      <div class="self-center">{tx.hash[1]}#{output.ref}</div>
-                      <div class="self-center">Addr {output.addr[1]}</div>
-                      {#if output.alias !== undefined}
-                        <div class="self-center">{output.alias}</div>
-                      {/if}
-                      <div class="self-center">&#x20B3; {output.value["ada"]} {#if output.tokenCount > 0}+ token(s){/if}</div>
-                    </a>
+                    Genesis transaction 
                   {/if}
-                {/each}
-              </div>
-            </td>
+                </div>
+              </td>
+              <td>
+                <div class="flex flex-col">
+                  {#each tx.outputs as output}
+                    {#if output.spentBy === "unspent"}
+                      <a class="self-center card flex flex-col flex-nowrap p-2 color-unspent-utxo shadow-xl mb-2" href="/utxo/{tx.id}/{output.ref}">
+                        <div class="self-center">{tx.hash[1]}#{output.ref}</div>
+                        <div class="self-center">Addr {output.addr[1]}</div>
+                        {#if output.alias !== undefined}
+                          <div class="self-center">{output.alias}</div>
+                        {/if}
+                        <div class="self-center">&#x20B3; {output.value["ada"]} {#if output.tokenCount > 0}+ token(s){/if}</div>
+                      </a>
+                    {:else}
+                      <a class="self-center card flex flex-col p-2 color-spent-utxo shadow-xl mb-2" href="/utxo/{tx.id}/{output.ref}">
+                        <div class="self-center">{tx.hash[1]}#{output.ref}</div>
+                        <div class="self-center">Addr {output.addr[1]}</div>
+                        {#if output.alias !== undefined}
+                          <div class="self-center">{output.alias}</div>
+                        {/if}
+                        <div class="self-center">&#x20B3; {output.value["ada"]} {#if output.tokenCount > 0}+ token(s){/if}</div>
+                      </a>
+                    {/if}
+                  {/each}
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
       </td>
